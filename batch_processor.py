9e7ace3a06b795b12dcde9df0d19dcfd347b1599
vladtsepesh <vladtsepesh.wow@gmail.com>
2025-08-12 17:44:07 +0300

    adding additional metadata_writer

--- batch_processor.py
+++ batch_processor.py
@@ -12,29 +12,45 @@ class BatchProcessor:
         self.encoder = encoder
         self.generator = generator
         self.writer = writer
+        self.supported_ext = {".jpg", ".jpeg", ".png"}
+        self.files = None
 
     def process_path(self):
-        supported_ext = {".jpg", ".jpeg", ".png"}
         if os.path.isdir(self.path):
-            for filename in os.listdir(self.path):
-                if os.path.splitext(filename.lower())[1] in supported_ext:
-                    file_path = os.path.join(self.path, filename)
-                    self.process(file_path, filename)
+            self.files = [os.path.join(self.path, f) for f in os.listdir(self.path)]
+        elif os.path.isfile(self.path):
+            self.files = [self.path]
+        else:
+            print(f"Invalid path: {self.path}")
+            return
 
-    def process(self, file_path, filename):
-        try:
-            print(f"Processing {file_path} ...")
-            base64_image = self.encoder.encode(file_path)
-            metadata = self.generator.generate(base64_image)
-            output_dir = os.path.join(self.path, "..", "images_with_metadata")
-            output_path = os.path.join(output_dir, filename)
-            self.writer.write_metadata(
-                file_path,
-                metadata['title'],
-                metadata['description'],
-                metadata['keywords'],
-                output_path
-            )
-            print(f"Saved with metadata: {output_path}")
-        except Exception as e:
-            print(f"Error processing {file_path}: {e}")
+    def _is_supported(self, file_path):
+        extension = os.path.splitext(file_path.lower())[1]
+        return extension in self.supported_ext
+
+    def _process_file(self):
+        for file_path in self.files:
+            if self._is_supported(file_path):
+
+                try:
+                    print(f"Processing {file_path} ...")
+                    base64_image = self.encoder.encode(file_path)
+                    metadata = self.generator.generate(base64_image)
+                    extension = os.path.splitext(file_path.lower())[1]
+                    output_dir = os.path.join(os.path.dirname(file_path), "..", "images_with_metadata")
+                    os.makedirs(output_dir, exist_ok=True)
+                    output_path = os.path.join(output_dir, f"{metadata['title']}{extension}")
+                    self.writer.write_metadata(
+                        file_path,
+                        metadata['title'],
+                        metadata['description'],
+                        metadata['keywords'],
+                        output_path
+                    )
+                    print(f"Saved with metadata: {output_path}")
+                except Exception as e:
+                    print(f"Error processing {file_path}: {e}")
+
+    def process(self):
+        self.process_path()
+        self._process_file()
